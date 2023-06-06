f08e806d5b76a71a8ee0209bd3b671cad114ca48
thetripleacoder <85655422+thetripleacoder@users.noreply.github.com>
2023-06-06 12:55:09 +0800

    1.5: course information steps 1-5

---
+++ part1/courseInformation/src/Content.js
@@ -0,0 +1,17 @@
+const Content = ({ parts }) => {
+  return (
+    <div>
+      <p>
+        {parts[0].name} {parts[0].exercises}
+      </p>
+      <p>
+        {parts[1].name} {parts[1].exercises}
+      </p>
+      <p>
+        {parts[2].name} {parts[2].exercises}
+      </p>
+    </div>
+  );
+};
+
+export default Content;
