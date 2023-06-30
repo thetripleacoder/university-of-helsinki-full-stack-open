838c7266b42b4d84d8753dd4e80f30fbd1d4e973
thetripleacoder <85655422+thetripleacoder@users.noreply.github.com>
2023-06-30 16:49:37 +0800

    Part 2 Altering data in server Exercises 2.12.-2.15.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
+import notesService from './services/persons';
 
 const App = () => {
   const [persons, setPersons] = useState([]);
@@ -11,22 +12,40 @@ const App = () => {
   const [filteredList, setFilteredList] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:3001/persons').then((response) => {
-      console.log('promise fulfilled', response);
-      setPersons(response.data);
+    notesService.getAll().then((response) => {
+      setPersons(response);
     });
   }, []);
 
   function addNumber(e) {
     e.preventDefault();
     if (persons.some((person) => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`);
+      // alert(`${newName} is already added to phonebook`);
+      let personIndex = persons.findIndex((person) => person.name === newName);
+      let updatedPersonData = {
+        name: newName,
+        number: newNumber,
+        id: personIndex + 1,
+      };
+      if (
+        window.confirm(
+          `${updatedPersonData.name} is already added to phonebook, replace the old number with a new one?`
+        )
+      ) {
+        notesService.update(updatedPersonData.id, updatedPersonData);
+        let copyPersons = persons;
+        copyPersons[personIndex] = updatedPersonData;
+        setPersons(copyPersons);
+        setNewName('');
+        setNewNumber('');
+      }
     } else {
       let newPerson = {
         name: newName,
         number: newNumber,
         id: persons.length + 1,
       };
+      notesService.create(newPerson);
       setPersons(persons.concat(newPerson));
       setNewName('');
       setNewNumber('');
@@ -39,6 +58,15 @@ const App = () => {
       person.name.toLowerCase().includes(keyword.toLowerCase())
     );
     setFilteredList(newFilteredList);
+  }
+
+  function deleteNumber(personData) {
+    if (window.confirm(`Delete ${personData.name} ?`)) {
+      notesService.deleteNumber(personData.id);
+      let copyPersons = persons;
+      copyPersons = copyPersons.filter((person) => person.id !== personData.id);
+      setPersons(copyPersons);
+    }
   }
 
   return (
@@ -59,7 +87,11 @@ const App = () => {
 
       <h3>Numbers</h3>
 
-      <Persons persons={persons} filteredList={filteredList} />
+      <Persons
+        persons={persons}
+        filteredList={filteredList}
+        handleDelete={deleteNumber}
+      />
     </div>
   );
 };
--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -1,9 +1,10 @@
-const Persons = ({ persons, filteredList }) => {
+const Persons = ({ persons, filteredList, handleDelete }) => {
   return (filteredList.length ? filteredList : persons).map((person) => {
     return (
       <p key={person.id}>
         <span>{person.name}</span>
         <span> {person.number}</span>
+        <button onClick={() => handleDelete(person)}>delete</button>
       </p>
     );
   });
